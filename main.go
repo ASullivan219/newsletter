5c1b971fb09b44cffc260af968899f8acbe739f6
Alex <a.sullivan219@protonmail.com>
2024-04-03 13:12:00 -0700

    added production listen and serve on tls

--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"components"
+	"fmt"
 	"net/http"
 
 	"github.com/a-h/templ"
 )
 
-func home(w http.ResponseWriter, r *http.Request)  {
-	
-}
-
+const (
+	inProduction = false
+)
 
 func main()  {
 
 	helloHandler := components.Hello()
 	http.Handle("/", templ.Handler(helloHandler))
-	http.ListenAndServe(":8080", nil)
+
+	if (!inProduction){
+		fmt.Println("Not production serving on port 8080:")
+		http.ListenAndServe(":8080", nil)
+	} else {
+		fmt.Println("Production server listening")
+		http.ListenAndServeTLS(":8080","./certs/cert.pem", "./certs/privateKey.key" , nil)
+	}
 
 }
